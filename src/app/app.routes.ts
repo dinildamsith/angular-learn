66014855d05debf8a0fbe6a02a9939a6543a728d
dinildamsith <dinildamsith52@gmail.com>
2025-04-12 01:15:57 +0530

    user component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
-import { Routes } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import HomeComponent from './pages/home/home.component';
+import UserComponent from './pages/users/user.component';
+
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent }, // When you go to /home 👉 show HomeComponent.
-
+  { path: 'users', component: UserComponent }, // When you go to /user👉 show UserComponent.
   { path: '', redirectTo: '/home', pathMatch: 'full'} // When you go to / (empty path) 👉 redirect to /home.
 ];
+
