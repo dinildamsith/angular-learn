8f09ea019dab6d15c66b87cae38bd1eb3f321dd1
dinildamsith <dinildamsith52@gmail.com>
2025-04-12 01:06:22 +0530

    home component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import HomeComponent from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent }, // When you go to /home 👉 show HomeComponent.
+
+  { path: '', redirectTo: '/home', pathMatch: 'full'} // When you go to / (empty path) 👉 redirect to /home.
+];
--- src/app/components/navBar/nav.component.ts
+++ src/app/components/navBar/nav.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 
 @Component({
   selector: 'nav-bar',
   standalone:true,
   templateUrl: 'nav.component.html',
-  styleUrl:'nav.component.css'
+  styleUrls: ['nav.component.css'],
+  imports: [RouterLink]
 })
 
 export class NavComponent{}
