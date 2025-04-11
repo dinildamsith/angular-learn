18cb1e96fa7bdc5c0bdb8b4cdb263df26d3dabaf
dinildamsith <dinildamsith52@gmail.com>
2025-04-12 00:50:11 +0530

    nav component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavComponent} from './components/navBar/nav.component';
 
 @Component({
-  selector: 'app-root',
-   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+   selector: 'app-root',
+   standalone: true,
+   templateUrl: './app.component.html',
+   imports: [RouterOutlet, NavComponent],
 })
 export class AppComponent {
   title = 'angular-learn';
---
+++ src/app/components/navBar/nav.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'nav-bar',
+  standalone:true,
+  templateUrl: 'nav.component.html',
+  styleUrl:'nav.component.css'
+})
+
+export class NavComponent{}
