56b1ef3d4458b428ff88817eaff1b301067448c4
dinildamsith <dinildamsith52@gmail.com>
2025-04-13 09:31:39 +0530

    constructor and ngOnInit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Logger} from '@angular/compiler-cli';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  templateUrl: 'home.component.html'
+  templateUrl: 'home.component.html',
 })
 
-export default class HomeComponent {}
+export default class HomeComponent implements OnInit {
+
+
+  // ----------  component create time this run
+  constructor() {
+    console.log("Home Component Created!....")
+  }
+
+
+  // --------- ngOnInit runs after component loads
+  ngOnInit() {
+    console.log('ngOnInit called');
+  }
+
+
+}
