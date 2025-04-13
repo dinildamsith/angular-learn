41059f5a1ab50293e1b76cfc2e7107a7413bd159
dinildamsith <dinildamsith52@gmail.com>
2025-04-13 09:39:50 +0530

    user service created

--- src/app/pages/users/user.component.ts
+++ src/app/pages/users/user.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import {UserService} from '../../services/users/user.service';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  templateUrl: 'user.component.html'
+  templateUrl: 'user.component.html',
+  providers: [UserService]
 })
 
-export default class UserComponent {}
+export default class UserComponent {
+
+  constructor(private userService : UserService) {
+    this.userService.userLogs("User Component Initialized....")
+  }
+
+}
---
+++ src/app/services/users/user.service.ts
@@ -0,0 +1,5 @@
+export class UserService {
+    userLogs(message: string) {
+      console.log("Logger -----------------------", message)
+    }
+}
