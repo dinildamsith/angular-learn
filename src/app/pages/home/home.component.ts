4e4a9576aa998fb0e4d150ee5b63e32999c0e5e9
dinildamsith <dinildamsith52@gmail.com>
2025-04-27 15:40:37 +0530

    added

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,22 @@
 import {Component, OnInit} from '@angular/core';
 import {Logger} from '@angular/compiler-cli';
 import {Test} from '../../services/users/user.service';
+import {NgIf} from '@angular/common';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: 'home.component.html',
+  imports: [
+    NgIf
+  ]
 })
 
 export default class HomeComponent implements OnInit {
+
+  isLoggedIn = true
+
 
 
   // ----------  component create time this run
